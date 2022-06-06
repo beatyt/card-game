2cac339d6115667463395fe782ff273b27a5ab67
beatyt <beatyt@gmail.com>
2022-06-05 20:01:16 -0400

    game phases

--- src/api/GamePhase.ts
+++ src/api/GamePhase.ts
@@ -1,7 +1,22 @@
 enum GamePhase {
   Initializing = "Initializing", 
   Initialized = "Initialized", 
-  Started = "Started", 
+  Started = "Started",
+  /**
+   * Mulligans
+   */
+  SetupStart = "Setup:Start", 
+  /**
+   * Cards drawn into hands, next is to start the turns with first player
+   */
+  SetupEnd = "Setup:End",
+  /**
+   * TurnPhases
+   */
+  Playing = "Playing",
+  /**
+   * Over
+   */
   Ended = "Ended"
 }
 
